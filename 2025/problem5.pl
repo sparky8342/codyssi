fcf8bcc5101b750fad2fa1ec3e81e0283123000d
David Bell <david.bell@gmail.com>
2025-03-22 11:26:11 +0100

    2025 problem 5 part 3

--- 2025/problem5.pl
+++ 2025/problem5.pl
@@ -16,6 +16,21 @@ sub calc_offset {
     return $offset;
 }
 
+sub calc_offset_pairs {
+    my ( $nums, $symbols ) = @_;
+    my $offset = $nums->[0] * 10 + $nums->[1];
+    for ( my $i = 2 ; $i < scalar @$nums ; $i += 2 ) {
+        my $num = $nums->[$i] * 10 + $nums->[ $i + 1 ];
+        if ( $symbols->[ $i / 2 - 1 ] eq '+' ) {
+            $offset += $num;
+        }
+        else {
+            $offset -= $num;
+        }
+    }
+    return $offset;
+}
+
 open my $fh, "<", "inputs/view_problem_5_input" or die "$!";
 chomp( my @nums = <$fh> );
 close $fh;
@@ -29,4 +44,8 @@ print "$offset\n";
 # part 2
 @symbols = reverse @symbols;
 $offset  = calc_offset( \@nums, \@symbols );
+print "$offset\n";
+
+# part 3
+$offset = calc_offset_pairs( \@nums, \@symbols );
 print "$offset\n";
