c7039611fbc74ba5eee3fbfc76706345cf90ba40
David Bell <david.bell@gmail.com>
2025-03-22 13:44:30 +0100

    refactor

--- 2025/problem7.pl
+++ 2025/problem7.pl
@@ -2,43 +2,64 @@
 use strict;
 use warnings;
 
-sub range_total {
-    my ($range) = @_;
-    my ( $start, $end ) = split /\-/, $range;
-    return $end - $start + 1;
-}
-
 sub max {
     return $_[0] > $_[1] ? $_[0] : $_[1];
+}
+
+sub ranges_cross {
+    my ( $range_a, $range_b ) = @_;
+    if ( $range_a->[0] > $range_b->[0] ) {
+        ( $range_a, $range_b ) = ( $range_b, $range_a );
+    }
+    if ( $range_a->[1] >= $range_b->[0] ) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+sub range_combine {
+    my ( $range_a, $range_b ) = @_;
+    if ( $range_a->[0] > $range_b->[0] ) {
+        ( $range_a, $range_b ) = ( $range_b, $range_a );
+    }
+    return [ $range_a->[0], max( $range_a->[1], $range_b->[1] ) ];
 }
 
 open my $fh, "<", "inputs/view_problem_7_input" or die "$!";
 chomp( my @lines = <$fh> );
 close $fh;
 
+my @ranges;
+for my $line (@lines) {
+    $line =~ /^(\d+)\-(\d+) (\d+)\-(\d+)$/;
+    my $range_a = [ $1, $2 ];
+    my $range_b = [ $3, $4 ];
+    push @ranges, [ $range_a, $range_b ];
+}
+
 # part 1
 my $total = 0;
-for my $line (@lines) {
-    my @ranges = split /\s/, $line;
-    $total += range_total( $ranges[0] );
-    $total += range_total( $ranges[1] );
+for my $pair (@ranges) {
+    my ( $range_a, $range_b ) = @$pair;
+    $total += $range_a->[1] - $range_a->[0] + 1;
+    $total += $range_b->[1] - $range_b->[0] + 1;
 }
 print "$total\n";
 
 # part 2
 $total = 0;
-for my $line (@lines) {
-    $line =~ /^(\d+)\-(\d+) (\d+)\-(\d+)$/;
-    my ( $r1, $r2, $r3, $r4 ) = ( $1, $2, $3, $4 );
-    if ( $r1 > $r3 ) {
-        ( $r1, $r2, $r3, $r4 ) = ( $r3, $r4, $r1, $r2 );
-    }
-    if ( $r2 >= $r3 ) {
-        $total += max( $r2, $r4 ) - $r1 + 1;
+for my $pair (@ranges) {
+    my ( $range_a, $range_b ) = @$pair;
+
+    if ( ranges_cross( $range_a, $range_b ) ) {
+        my $range = range_combine( $range_a, $range_b );
+        $total += $range->[1] - $range->[0] + 1;
     }
     else {
-        $total += $r2 - $r1 + 1;
-        $total += $r4 - $r3 + 1;
+        $total += $range_a->[1] - $range_a->[0] + 1;
+        $total += $range_b->[1] - $range_b->[0] + 1;
     }
 }
 print "$total\n";
