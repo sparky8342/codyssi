42d7a29cc091fbf2b6fde99865d023ce84715c47
David Bell <david.bell@gmail.com>
2025-03-22 14:24:40 +0100

    2025 problem 7 part 3

--- 2025/problem7.pl
+++ 2025/problem7.pl
@@ -27,6 +27,27 @@ sub range_combine {
     return [ $range_a->[0], max( $range_a->[1], $range_b->[1] ) ];
 }
 
+sub range_multi_combine {
+    my ($ranges) = @_;
+
+    my $new_ranges = [];
+
+  outer:
+    while ( scalar @$ranges ) {
+        my $range = shift @$ranges;
+        for ( my $i = 0 ; $i < scalar @$ranges ; $i++ ) {
+            if ( ranges_cross( $ranges->[$i], $range ) ) {
+                my $combined = range_combine( $ranges->[$i], $range );
+                splice( @$ranges, $i, 1, $combined );
+                next outer;
+            }
+        }
+        push @$new_ranges, $range;
+    }
+
+    return $new_ranges;
+}
+
 open my $fh, "<", "inputs/view_problem_7_input" or die "$!";
 chomp( my @lines = <$fh> );
 close $fh;
@@ -63,3 +84,23 @@ for my $pair (@ranges) {
     }
 }
 print "$total\n";
+
+# part 3
+my $best = 0;
+for ( my $i = 0 ; $i < scalar @ranges - 1 ; $i++ ) {
+    my ( $range_a, $range_b ) = @{ $ranges[$i] };
+    my ( $range_c, $range_d ) = @{ $ranges[ $i + 1 ] };
+
+    my $group =
+      range_multi_combine( [ $range_a, $range_b, $range_c, $range_d ] );
+
+    my $total = 0;
+    for my $range (@$group) {
+        my ( $start, $end ) = @$range;
+        $total += $end - $start + 1;
+    }
+    if ( $total > $best ) {
+        $best = $total;
+    }
+}
+print "$best\n";
