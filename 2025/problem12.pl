9cc2385aebf9ea83b6d3aae1a52e872716c3236e
David Bell <david.bell@gmail.com>
2025-03-24 12:50:10 +0100

    2025 problem 12 parts 2 and 3

--- 2025/problem12.pl
+++ 2025/problem12.pl
@@ -3,9 +3,12 @@ use strict;
 use warnings;
 
 open my $fh, "<", "inputs/view_problem_12_input" or die "$!";
+
+#open my $fh, "<", "p12.txt" or die "$!";
 chomp( my @lines = <$fh> );
 close $fh;
 
+# part 1
 my $ab = 0;
 for my $line (@lines) {
     for my $char ( split //, $line ) {
@@ -17,3 +20,21 @@ for my $line (@lines) {
     }
 }
 print "$ab\n";
+
+# part 2
+my $count = 0;
+for my $line (@lines) {
+    my $copy = $line;
+    1 while $copy =~ s/\d\D|\D\d//g;
+    $count += length($copy);
+}
+print "$count\n";
+
+# part 3
+$count = 0;
+for my $line (@lines) {
+    my $copy = $line;
+    1 while $copy =~ s/\d[a-zA-Z]|[a-zA-Z]\d//g;
+    $count += length($copy);
+}
+print "$count\n";
