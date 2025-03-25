f0a25baaf2630e741e2599e63a97e00493888584
David Bell <david.bell@gmail.com>
2025-03-25 12:27:03 +0000

    2025 problem 13 part 2

--- 2025/problem13.pl
+++ 2025/problem13.pl
@@ -2,11 +2,23 @@
 use strict;
 use warnings;
 
+sub run_transactions {
+    my ( $people, $transactions, $no_negative ) = @_;
+    for my $transaction (@$transactions) {
+        my ( $from, $to, $amount ) = @$transaction;
+        if ( $no_negative && $people->{$from} < $amount ) {
+            $amount = $people->{$from};
+        }
+        $people->{$from} -= $amount;
+        $people->{$to}   += $amount;
+    }
+}
+
 open my $fh, "<", "inputs/view_problem_13_input" or die "$!";
 chomp( my @lines = <$fh> );
 close $fh;
 
-# part 1
+# parse data
 my %people;
 my $i;
 for ( $i = 0 ; $i < scalar @lines ; $i++ ) {
@@ -18,11 +30,20 @@ for ( $i = 0 ; $i < scalar @lines ; $i++ ) {
     }
 }
 $i++;
+my @transactions;
 for ( ; $i < scalar @lines ; $i++ ) {
     $lines[$i] =~ /^FROM ([A-Za-z-]+) TO ([A-Za-z-]+) AMT (\d+)$/;
-    $people{$1} -= $3;
-    $people{$2} += $3;
+    push @transactions, [ $1, $2, $3 ];
 }
 
-my @balances = sort { $b <=> $a } values %people;
+# part 1
+my $people_copy = {%people};
+run_transactions( $people_copy, \@transactions );
+my @balances = sort { $b <=> $a } values %$people_copy;
+print $balances[0] + $balances[1] + $balances[2] . "\n";
+
+# part 2
+$people_copy = {%people};
+run_transactions( $people_copy, \@transactions, 1 );
+@balances = sort { $b <=> $a } values %$people_copy;
 print $balances[0] + $balances[1] + $balances[2] . "\n";
