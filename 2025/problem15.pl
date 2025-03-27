e6219216027b9cf40c36ca8fddd0c49c8149da7f
David Bell <david.bell@gmail.com>
2025-03-27 12:12:11 +0000

    2025 problem 15 parts 1 and 2

---
+++ 2025/problem15.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub base_from {
+    my ( $num, $base ) = @_;
+    my $result = 0;
+    for my $digit ( split( //, $num ) ) {
+        $result =
+          $base * $result +
+          index(
+            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+            $digit );
+    }
+    return $result;
+}
+
+sub base_to {
+    my ( $num, $base ) = @_;
+    my $result = "";
+    do {
+        $result =
+          ( '0' .. '9', 'A' .. 'Z', 'a' .. 'z', '!', '@', '#', '$', '%', '^' )
+          [ $num % $base ] . $result;
+    } while $num = int( $num / $base );
+    return $result;
+}
+
+open my $fh, "<", "inputs/view_problem_15_input" or die "$!";
+#open my $fh, "<", "p15.txt" or die "$!";
+chomp( my @lines = <$fh> );
+close $fh;
+
+my @nums = map { [ split /\s/, $_ ] } @lines;
+
+# part 1
+my $max = 0;
+foreach my $num (@nums) {
+    my ( $n, $base ) = @$num;
+    my $base10 = base_from( $n, $base );
+    if ( $base10 > $max ) {
+        $max = $base10;
+    }
+}
+print "$max\n";
+
+# part 2
+my $sum = 0;
+foreach my $num (@nums) {
+    my ( $n, $base ) = @$num;
+    $sum += base_from( $n, $base );
+}
+print base_to( $sum, 68 ) . "\n";
