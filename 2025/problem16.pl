42d8ce266a0b1443eaa4a327c29970a06f07460b
David Bell <david.bell@gmail.com>
2025-03-28 12:44:13 +0000

    2025 problem 16 part 2

--- 2025/problem16.pl
+++ 2025/problem16.pl
@@ -69,35 +69,9 @@ sub math_op_all {
     }
 }
 
-open my $fh, "<", "inputs/view_problem_16_input" or die "$!";
+sub execute_ins {
+    my ( $g, $line ) = @_;
 
-#open my $fh, "<", "test.txt" or die "$!";
-my $data = do { local $/; <$fh> };
-close $fh;
-
-my @parts = split /\n\n/, $data;
-
-my @grid;
-foreach my $line ( split /\n/, $parts[0] ) {
-    push @grid, [ split /\s/, $line ];
-}
-
-$size = scalar @grid;
-
-#ADD 60 ROW 25
-#SUB 79 COL 1
-#SHIFT ROW 30 BY 19
-#MULTIPLY 4 COL 19
-#SHIFT COL 29 BY 28
-#ADD 6 ROW 6
-#SHIFT COL 26 BY 5
-#SUB 98 ROW 17
-#MULTIPLY 2 ROW 1
-#SHIFT ROW 27 BY 19
-
-# part 1
-my $g = dclone( \@grid );
-foreach my $line ( split /\n/, $parts[1] ) {
     my @ins = split /\s/, $line;
     my $op  = $ins[0];
 
@@ -129,15 +103,68 @@ foreach my $line ( split /\n/, $parts[1] ) {
     }
 }
 
-my $max = 0;
-for ( my $i = 0 ; $i < $size ; $i++ ) {
-    my $r = 0;
-    my $c = 0;
-    for ( my $j = 0 ; $j < $size ; $j++ ) {
-        $r += $g->[$i][$j];
-        $c += $g->[$j][$i];
+sub max_row_col {
+    my ($g) = @_;
+    my $max = 0;
+    for ( my $i = 0 ; $i < $size ; $i++ ) {
+        my $r = 0;
+        my $c = 0;
+        for ( my $j = 0 ; $j < $size ; $j++ ) {
+            $r += $g->[$i][$j];
+            $c += $g->[$j][$i];
+        }
+        $max = max( $max, $r );
+        $max = max( $max, $c );
     }
-    $max = max( $max, $r );
-    $max = max( $max, $c );
+    return $max;
 }
-print "$max\n";
+
+open my $fh, "<", "inputs/view_problem_16_input" or die "$!";
+
+#open my $fh, "<", "test.txt" or die "$!";
+my $data = do { local $/; <$fh> };
+close $fh;
+
+my @parts = split /\n\n/, $data;
+
+my @grid;
+foreach my $line ( split /\n/, $parts[0] ) {
+    push @grid, [ split /\s/, $line ];
+}
+
+$size = scalar @grid;
+my @instructions = split /\n/, $parts[1];
+
+#ADD 60 ROW 25
+#SUB 79 COL 1
+#SHIFT ROW 30 BY 19
+#MULTIPLY 4 COL 19
+#SHIFT COL 29 BY 28
+#ADD 6 ROW 6
+#SHIFT COL 26 BY 5
+#SUB 98 ROW 17
+#MULTIPLY 2 ROW 1
+#SHIFT ROW 27 BY 19
+
+# part 1
+my $g = dclone( \@grid );
+foreach my $line (@instructions) {
+    execute_ins( $g, $line );
+}
+print max_row_col($g) . "\n";
+
+# part 2
+$g = dclone( \@grid );
+my $ins;
+foreach my $flow ( split /\n/, $parts[2] ) {
+    if ( $flow eq 'TAKE' ) {
+        $ins = shift @instructions;
+    }
+    elsif ( $flow eq 'CYCLE' ) {
+        push @instructions, $ins;
+    }
+    elsif ( $flow eq 'ACT' ) {
+        execute_ins( $g, $ins );
+    }
+}
+print max_row_col($g) . "\n";
