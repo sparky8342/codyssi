c3a6c7061888a3462c5c347f0fd37725b03e7e5c
David Bell <david.bell@gmail.com>
2025-03-29 13:04:19 +0000

    2025 problem 17 part 2

--- 2025/problem17.pl
+++ 2025/problem17.pl
@@ -4,6 +4,30 @@ use warnings;
 
 use Data::Dumper;
 
+sub bfs {
+    my ( $graph, $use_distances ) = @_;
+    my @queue   = ( { pos => 'STT', distance => 0 } );
+    my %visited = ( 'STT' => 1 );
+    my @distances;
+    while ( scalar @queue ) {
+        my $entry = shift @queue;
+        push @distances, $entry->{distance};
+        for my $neighbour ( @{ $graph->{ $entry->{pos} } } ) {
+            my ( $npos, $dist ) = @$neighbour;
+            if ( !exists( $visited{$npos} ) ) {
+                if ( !$use_distances ) {
+                    $dist = 1;
+                }
+                push @queue,
+                  { pos => $npos, distance => $entry->{distance} + $dist };
+                $visited{$npos} = 1;
+            }
+        }
+    }
+    @distances = sort { $b <=> $a } @distances;
+    return $distances[0] * $distances[1] * $distances[2];
+}
+
 open my $fh, "<", "inputs/view_problem_17_input" or die "$!";
 
 #open my $fh, "<", "test.txt" or die "$!";
@@ -11,26 +35,13 @@ chomp( my @lines = <$fh> );
 close $fh;
 
 my %graph;
-
 foreach my $line (@lines) {
     $line =~ /^(\w+)\s->\s(\w+)\s\|\s(\d+)$/;
     push @{ $graph{$1} }, [ $2, $3 ];
 }
 
 # part 1
-my @queue   = ( { pos => 'STT', distance => 0 } );
-my %visited = ( 'STT' => 1 );
-my @distances;
-while ( scalar @queue ) {
-    my $entry = shift @queue;
-    push @distances, $entry->{distance};
-    for my $neighbour ( @{ $graph{ $entry->{pos} } } ) {
-        my ($npos) = @$neighbour;
-        if ( !exists( $visited{$npos} ) ) {
-            push @queue, { pos => $npos, distance => $entry->{distance} + 1 };
-            $visited{$npos} = 1;
-        }
-    }
-}
-@distances = sort { $b <=> $a } @distances;
-print $distances[0] * $distances[1] * $distances[2] . "\n";
+print bfs( \%graph ) . "\n";
+
+# part 2
+print bfs( \%graph, 1 ) . "\n";
