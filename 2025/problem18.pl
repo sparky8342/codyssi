000abe8cbd59c569619ff561d50cb61aaad5f699
David Bell <david.bell@gmail.com>
2025-03-30 16:09:04 +0100

    2025 problem 18 part 2

--- 2025/problem18.pl
+++ 2025/problem18.pl
@@ -2,8 +2,6 @@
 use strict;
 use warnings;
 
-use Data::Dumper;
-
 open my $fh, "<", "inputs/view_problem_18_input" or die "$!";
 chomp( my @lines = <$fh> );
 close $fh;
@@ -23,3 +21,44 @@ for ( my $i = 0 ; $i < 5 ; $i++ ) {
     $total += $items[$i]->{materials};
 }
 print "$total\n";
+
+# part 2
+@items = sort { $a->{cost} <=> $b->{cost} } @items;
+
+my @dp = ( {} ) x 31;
+for ( my $i = 0 ; $i < scalar @items ; $i++ ) {
+    $dp[ $items[$i]->{cost} ] = {
+        quality   => $items[$i]->{quality},
+        items     => { $i => 1 },
+        materials => $items[$i]->{materials}
+    };
+}
+
+for ( my $i = 0 ; $i <= 30 ; $i++ ) {
+    if ( !exists( $dp[$i]->{quality} ) ) {
+        next;
+    }
+    for ( my $j = 0 ; $j < scalar @items ; $j++ ) {
+        if ( exists( $dp[$i]->{items}->{$j} ) ) {
+            next;
+        }
+        my $new_cost = $i + $items[$j]->{cost};
+        if ( $new_cost <= 30 ) {
+            if ( !exists( $dp[$new_cost]->{quality} )
+                || $dp[$new_cost]->{quality} <
+                $dp[$i]->{quality} + $items[$j]->{quality} )
+            {
+                my %new_items = %{ $dp[$i]->{items} };
+                $new_items{$j} = 1;
+                $dp[$new_cost] = {
+                    quality   => $dp[$i]->{quality} + $items[$j]->{quality},
+                    items     => \%new_items,
+                    materials => $dp[$i]->{materials} + $items[$j]->{materials}
+                };
+            }
+        }
+    }
+}
+
+print $dp[30]->{quality} * $dp[30]->{materials} . "\n";
+
