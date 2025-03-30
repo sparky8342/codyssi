fce168c2fbe7935fddccd663b1379d71510835a8
David Bell <david.bell@gmail.com>
2025-03-31 00:38:05 +0100

    more tidy up

--- 2025/problem18.pl
+++ 2025/problem18.pl
@@ -22,10 +22,9 @@ for ( my $i = 0 ; $i < 5 ; $i++ ) {
 }
 print "$total\n";
 
-# part 2
-@items = sort { $a->{cost} <=> $b->{cost} } @items;
-
-my $max_cost = 300;
+# parts 2 and 3
+my $part2_cost = 30;
+my $max_cost   = 300;
 
 my @dp = ( { quality => 0, materials => 0 } ) x ( $max_cost + 1 );
 for ( my $i = 0 ; $i < scalar @items ; $i++ ) {
@@ -44,34 +43,38 @@ for ( my $i = 0 ; $i <= $max_cost ; $i++ ) {
         if ( exists( $dp[$i]->{items}->{$j} ) ) {
             next;
         }
+
         my $new_cost = $i + $items[$j]->{cost};
+
         if ( $new_cost > $max_cost ) {
             next;
         }
 
-        my $existing_quality    = $dp[$new_cost]->{quality};
-        my $new_quality         = $dp[$i]->{quality} + $items[$j]->{quality};
-        my $existing_item_count = scalar keys %{ $dp[$new_cost]->{items} };
-        my $new_item_count      = ( scalar keys %{ $dp[$i]->{items} } ) + 1;
+        my $existing_quality = $dp[$new_cost]->{quality};
+        my $new_quality      = $dp[$i]->{quality} + $items[$j]->{quality};
 
         if ( $existing_quality > $new_quality ) {
             next;
         }
-        elsif ( $existing_quality == $new_quality ) {
-            if ( $existing_item_count < $new_item_count ) {
-                next;
-            }
+
+        my $existing_item_count = scalar keys %{ $dp[$new_cost]->{items} };
+        my $new_item_count      = ( scalar keys %{ $dp[$i]->{items} } ) + 1;
+
+        # not sure if this strategy will work with all inputs
+        if (   $existing_quality == $new_quality
+            && $existing_item_count < $new_item_count )
+        {
+            next;
         }
 
-        my %new_items = %{ $dp[$i]->{items} };
-        $new_items{$j} = 1;
         $dp[$new_cost] = {
             quality   => $dp[$i]->{quality} + $items[$j]->{quality},
-            items     => \%new_items,
-            materials => $dp[$i]->{materials} + $items[$j]->{materials}
+            items     => { ( %{ $dp[$i]->{items} }, ( $j => 1 ) ) },
+            materials => $dp[$i]->{materials} + $items[$j]->{materials},
         };
     }
 }
 
-print $dp[30]->{quality} * $dp[30]->{materials} . "\n";
-print $dp[300]->{quality} * $dp[300]->{materials} . "\n";
+printf( "%d\n%d\n",
+    $dp[$part2_cost]->{quality} * $dp[$part2_cost]->{materials},
+    $dp[$max_cost]->{quality} * $dp[$max_cost]->{materials} );
