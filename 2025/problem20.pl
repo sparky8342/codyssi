22e8b0d741500615560479f6cf44746939860b81
David Bell <david.bell@gmail.com>
2025-04-02 01:39:33 +0100

    some tidy up/refactor

--- 2025/problem20.pl
+++ 2025/problem20.pl
@@ -5,47 +5,51 @@ use List::Util qw(max);
 use Math::BigInt;
 use Storable qw(dclone);
 
-my $size = 80;
+use constant SIZE => 80;
 
-my %graph;
-
+# move from face id in one of four directions (A, B, C or D)
+# the value is the new face and any rotation to be made
+# (of the viewer)
+#
 # n = nothing, l = rotate left, r = rotate right, f = flip 180
-$graph{0} = {
-    'A' => [ 3, "n" ],
-    'B' => [ 5, "n" ],
-    'C' => [ 1, "n" ],
-    'D' => [ 4, "n" ],
-};
-$graph{1} = {
-    'A' => [ 0, "n" ],
-    'B' => [ 5, "r" ],
-    'C' => [ 2, "n" ],
-    'D' => [ 4, "l" ],
-};
-$graph{2} = {
-    'A' => [ 1, "n" ],
-    'B' => [ 5, "f" ],
-    'C' => [ 3, "n" ],
-    'D' => [ 4, "f" ],
-};
-$graph{3} = {
-    'A' => [ 2, "n" ],
-    'B' => [ 5, "l" ],
-    'C' => [ 0, "n" ],
-    'D' => [ 4, "r" ],
-};
-$graph{4} = {
-    'A' => [ 3, "l" ],
-    'B' => [ 0, "n" ],
-    'C' => [ 1, "r" ],
-    'D' => [ 2, "f" ],
-};
-$graph{5} = {
-    'A' => [ 3, "r" ],
-    'B' => [ 2, "f" ],
-    'C' => [ 1, "l" ],
-    'D' => [ 0, "n" ],
-};
+my %move_rotation = (
+    0 => {
+        'A' => [ 3, "n" ],
+        'B' => [ 5, "n" ],
+        'C' => [ 1, "n" ],
+        'D' => [ 4, "n" ],
+    },
+    1 => {
+        'A' => [ 0, "n" ],
+        'B' => [ 5, "r" ],
+        'C' => [ 2, "n" ],
+        'D' => [ 4, "l" ],
+    },
+    2 => {
+        'A' => [ 1, "n" ],
+        'B' => [ 5, "f" ],
+        'C' => [ 3, "n" ],
+        'D' => [ 4, "f" ],
+    },
+    3 => {
+        'A' => [ 2, "n" ],
+        'B' => [ 5, "l" ],
+        'C' => [ 0, "n" ],
+        'D' => [ 4, "r" ],
+    },
+    4 => {
+        'A' => [ 3, "l" ],
+        'B' => [ 0, "n" ],
+        'C' => [ 1, "r" ],
+        'D' => [ 2, "f" ],
+    },
+    5 => {
+        'A' => [ 3, "r" ],
+        'B' => [ 2, "f" ],
+        'C' => [ 1, "l" ],
+        'D' => [ 0, "n" ],
+    }
+);
 
 sub process_commands {
     my ( $faces, $lines, $turns, $wrap_around ) = @_;
@@ -62,29 +66,21 @@ sub process_commands {
         my $line = $lines->[$i];
         if ( $line =~ /^FACE - VALUE (\d+)$/ ) {
             my $n = $1;
-            $absorptions[$current_face] += $size * $size * $n;
-            for ( my $y = 0 ; $y < $size ; $y++ ) {
-                for ( my $x = 0 ; $x < $size ; $x++ ) {
-                    $faces->[$current_face][$y][$x] += $n;
-                    while ( $faces->[$current_face][$y][$x] > 100 ) {
-                        $faces->[$current_face][$y][$x] -= 100;
-                    }
-
-                }
-            }
+            $absorptions[$current_face] += SIZE * SIZE * $n;
+            update_all( $faces->[$current_face], $n );
         }
         elsif ( $line =~ /^ROW (\d+) - VALUE (\d+)$/ ) {
             my ( $row, $n ) = ( $1, $2 );
-            $absorptions[$current_face] += $size * $n;
-            update_row( $faces, $current_face, $current_dirs, $row, $n );
+            $absorptions[$current_face] += SIZE * $n;
+            update_row( $faces->[$current_face], $current_dirs, $row, $n );
             if ($wrap_around) {
                 my $dir = 'R';
 
                 for ( 1 .. 3 ) {
                     ( $current_face, $current_dirs ) =
                       rotate( $current_face, $current_dirs, $dir );
-                    update_row( $faces, $current_face, $current_dirs, $row,
-                        $n );
+                    update_row( $faces->[$current_face],
+                        $current_dirs, $row, $n );
                 }
                 ( $current_face, $current_dirs ) =
                   rotate( $current_face, $current_dirs, $dir );
@@ -92,16 +88,16 @@ sub process_commands {
         }
         elsif ( $line =~ /^COL (\d+) - VALUE (\d+)$/ ) {
             my ( $col, $n ) = ( $1, $2 );
-            $absorptions[$current_face] += $size * $n;
-            update_col( $faces, $current_face, $current_dirs, $col, $n );
+            $absorptions[$current_face] += SIZE * $n;
+            update_col( $faces->[$current_face], $current_dirs, $col, $n );
             if ($wrap_around) {
                 my $dir = 'U';
 
                 for ( 1 .. 3 ) {
                     ( $current_face, $current_dirs ) =
                       rotate( $current_face, $current_dirs, $dir );
-                    update_col( $faces, $current_face, $current_dirs, $col,
-                        $n );
+                    update_col( $faces->[$current_face],
+                        $current_dirs, $col, $n );
                 }
                 ( $current_face, $current_dirs ) =
                   rotate( $current_face, $current_dirs, $dir );
@@ -130,7 +126,8 @@ sub rotate {
 
     my $letter_dir = $current_dirs->{$rotation};
     my $dir_change;
-    ( $current_face, $dir_change ) = @{ $graph{$current_face}->{$letter_dir} };
+    ( $current_face, $dir_change ) =
+      @{ $move_rotation{$current_face}->{$letter_dir} };
 
     if ( $dir_change eq 'l' ) {
         $current_dirs = {
@@ -160,58 +157,64 @@ sub rotate {
     return ( $current_face, $current_dirs );
 }
 
+sub add {
+    my ( $num, $n ) = @_;
+    $num += $n;
+    while ( $num > 100 ) {
+        $num -= 100;
+    }
+    return $num;
+}
+
+sub update_all {
+    my ( $face, $n ) = @_;
+    for ( my $y = 0 ; $y < SIZE ; $y++ ) {
+        for ( my $x = 0 ; $x < SIZE ; $x++ ) {
+            $face->[$y][$x] = add( $face->[$y][$x], $n );
+        }
+    }
+}
+
 sub update_row {
-    my ( $faces, $current_face, $current_dirs, $r, $n ) = @_;
+    my ( $face, $current_dirs, $r, $n ) = @_;
 
     my $up = $current_dirs->{'U'};
     $r--;
 
     if ( $up eq 'C' || $up eq 'B' ) {
-        $r = $size - $r - 1;
+        $r = SIZE - $r - 1;
     }
 
     if ( $up eq 'A' || $up eq 'C' ) {
-        for ( my $col = 0 ; $col < $size ; $col++ ) {
-            $faces->[$current_face][$r][$col] += $n;
-            while ( $faces->[$current_face][$r][$col] > 100 ) {
-                $faces->[$current_face][$r][$col] -= 100;
-            }
+        for ( my $col = 0 ; $col < SIZE ; $col++ ) {
+            $face->[$r][$col] = add( $face->[$r][$col], $n );
         }
     }
     elsif ( $up eq 'B' || $up eq 'D' ) {
-        for ( my $row = 0 ; $row < $size ; $row++ ) {
-            $faces->[$current_face][$row][$r] += $n;
-            while ( $faces->[$current_face][$row][$r] > 100 ) {
-                $faces->[$current_face][$row][$r] -= 100;
-            }
+        for ( my $row = 0 ; $row < SIZE ; $row++ ) {
+            $face->[$row][$r] = add( $face->[$row][$r], $n );
         }
     }
 }
 
 sub update_col {
-    my ( $faces, $current_face, $current_dirs, $c, $n ) = @_;
+    my ( $face, $current_dirs, $c, $n ) = @_;
 
     my $up = $current_dirs->{'U'};
     $c--;
 
     if ( $up eq 'C' || $up eq 'D' ) {
-        $c = $size - $c - 1;
+        $c = SIZE - $c - 1;
     }
 
     if ( $up eq 'A' || $up eq 'C' ) {
-        for ( my $row = 0 ; $row < $size ; $row++ ) {
-            $faces->[$current_face][$row][$c] += $n;
-            while ( $faces->[$current_face][$row][$c] > 100 ) {
-                $faces->[$current_face][$row][$c] -= 100;
-            }
+        for ( my $row = 0 ; $row < SIZE ; $row++ ) {
+            $face->[$row][$c] = add( $face->[$row][$c], $n );
         }
     }
     elsif ( $up eq 'B' || $up eq 'D' ) {
-        for ( my $col = 0 ; $col < $size ; $col++ ) {
-            $faces->[$current_face][$c][$col] += $n;
-            while ( $faces->[$current_face][$c][$col] > 100 ) {
-                $faces->[$current_face][$c][$col] -= 100;
-            }
+        for ( my $col = 0 ; $col < SIZE ; $col++ ) {
+            $face->[$c][$col] = add( $face->[$c][$col], $n );
         }
     }
 }
@@ -220,10 +223,10 @@ sub max_row_col {
     my ($face) = @_;
 
     my $max = 0;
-    for ( my $i = 0 ; $i < $size ; $i++ ) {
+    for ( my $i = 0 ; $i < SIZE ; $i++ ) {
         my $r = 0;
         my $c = 0;
-        for ( my $j = 0 ; $j < $size ; $j++ ) {
+        for ( my $j = 0 ; $j < SIZE ; $j++ ) {
             $r += $face->[$i][$j];
             $c += $face->[$j][$i];
         }
@@ -244,22 +247,20 @@ pop @lines;
 my @faces;
 for ( 1 .. 6 ) {
     my @face;
-    for ( 1 .. $size ) {
-        my @row;
-        for ( 1 .. $size ) {
-            push @row, 1;
-        }
+    for ( 1 .. SIZE ) {
+        my @row = (1) x SIZE;
         push @face, \@row;
     }
     push @faces, \@face;
 }
 
+# parts 1 and 2
 my ( $absorption_product, $product ) =
   process_commands( dclone( \@faces ), \@lines, \@turns );
-print "$absorption_product\n";
-print "$product\n";
+print "$absorption_product\n$product\n";
 
-( $absorption_product, $product ) =
+# part 3
+( undef, $product ) =
   process_commands( dclone( \@faces ), \@lines, \@turns, 1 );
 print "$product\n";
 
