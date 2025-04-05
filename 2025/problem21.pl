dc8dba9ca59baed23660e8a9b4fc201c7fed825c
David Bell <david.bell@gmail.com>
2025-04-05 19:13:27 +0100

    updated to use hash of edges instead of actually linking all nodes together

--- 2025/problem21.pl
+++ 2025/problem21.pl
@@ -3,61 +3,63 @@ use strict;
 use warnings;
 use Math::BigInt;
 
+sub add_edge {
+    my ( $edges, $from, $to ) = @_;
+    push @{ $edges->{$from} }, $to;
+}
+
 sub create_graph {
     my ($lines) = @_;
 
-    my %node_cache;    # for building the graph only
+    my $edges = {};
 
     $lines->[0] =~ /^S1 : 0 -> (\d+) : FROM START TO END$/;
     my $end = $1;
 
-    my $head = { id => "S1_0" };
-    $node_cache{"S1_0"} = $head;
-    my $node = $head;
+    my $node = "S1_0";
 
-    for ( 1 .. $end ) {
-        my $next_node = { id => "S1_$_" };
-        $node_cache{"S1_$_"} = $next_node;
-        $node->{next}        = [$next_node];
-        $node                = $node->{next}->[0];
+    for ( my $i = 0 ; $i < $end ; $i++ ) {
+        add_edge( $edges, sprintf( "S1_%d", $i ), sprintf( "S1_%d", $i + 1 ) );
     }
-
-    my $end_node = $node;
 
     for ( my $i = 1 ; $i < scalar @$lines ; $i++ ) {
         $lines->[$i] =~ /^S(\d+) : (\d+) -> (\d+) : FROM S(\d+) TO S(\d+)$/;
         my ( $id, $start, $end, $from_id, $to_id ) = ( $1, $2, $3, $4, $5 );
-        my $start_node = { id => "S$id" . "_" . $start };
-        $node_cache{ "S$id" . "_" . $start } = $start_node;
-        my $node = $start_node;
-        for ( my $j = $start + 1 ; $j <= $end ; $j++ ) {
-            my $next_node = { id => "S$id" . "_" . $j };
-            $node_cache{ "S$id" . "_" . $j } = $next_node;
-            $node->{next}                    = [$next_node];
-            $node                            = $node->{next}->[0];
+
+        add_edge(
+            $edges,
+            sprintf( "S%d_%d", $from_id, $start ),
+            sprintf( "S%d_%d", $id,      $start )
+        );
+
+        for ( my $i = $start ; $i < $end ; $i++ ) {
+            add_edge(
+                $edges,
+                sprintf( "S%d_%d", $id, $i ),
+                sprintf( "S%d_%d", $id, $i + 1 )
+            );
         }
 
-        # link up path
-        my $path_node = $node_cache{ "S$from_id" . "_" . $start };
-        unshift @{ $path_node->{next} }, $start_node;
-
-        $path_node = $node_cache{ "S$to_id" . "_" . $end };
-        push @{ $node->{next} }, $path_node;
+        add_edge(
+            $edges,
+            sprintf( "S%d_%d", $id,    $end ),
+            sprintf( "S%d_%d", $to_id, $end )
+        );
     }
 
-    return ( $head, $end_node );
+    return ( $edges, "S1_0", "S1_$end" );
 }
 
 sub dfs_steps {
-    my ( $node, $steps ) = @_;
+    my ( $edges, $node, $steps ) = @_;
 
     if ( $steps == 0 ) {
         return [$node];
     }
 
     my @nodes;
-    for my $next_node ( @{ $node->{next} } ) {
-        my $n = dfs_steps( $next_node, $steps - 1 );
+    for my $next_node ( @{ $edges->{$node} } ) {
+        my $n = dfs_steps( $edges, $next_node, $steps - 1 );
         @nodes = ( @nodes, @$n );
     }
 
@@ -65,65 +67,65 @@ sub dfs_steps {
 }
 
 sub get_next_nodes {
-    my ( $node, $moves ) = @_;
+    my ( $edges, $node, $moves ) = @_;
 
     my @next_nodes;
     for my $move (@$moves) {
-        @next_nodes = ( @next_nodes, @{ dfs_steps( $node, $move ) } );
+        @next_nodes = ( @next_nodes, @{ dfs_steps( $edges, $node, $move ) } );
     }
 
-    my %n = map { $_->{id} => $_ } @next_nodes;
-    @next_nodes = values %n;
+    my %n = map { $_ => 1 } @next_nodes;
+    @next_nodes = keys %n;
 
     return \@next_nodes;
 }
 
 sub dfs {
-    my ( $node, $end_node, $moves, $cache ) = @_;
+    my ( $edges, $node, $end_node, $moves, $cache ) = @_;
 
-    if ( exists( $cache->{ $node->{id} } ) ) {
-        return $cache->{ $node->{id} };
+    if ( exists( $cache->{$node} ) ) {
+        return $cache->{$node};
     }
 
-    if ( $node->{id} eq $end_node->{id} ) {
+    if ( $node eq $end_node ) {
         return 1;
     }
 
-    my $next_nodes = get_next_nodes( $node, $moves );
+    my $next_nodes = get_next_nodes( $edges, $node, $moves );
 
     my $paths = Math::BigInt->new(0);
     for my $next_node (@$next_nodes) {
-        $paths += dfs( $next_node, $end_node, $moves, $cache );
+        $paths += dfs( $edges, $next_node, $end_node, $moves, $cache );
     }
 
-    $cache->{ $node->{id} } = $paths;
+    $cache->{$node} = $paths;
     return $paths;
 }
 
 sub find_path_no {
-    my ( $head, $end_node, $moves, $target, $min, $max ) = @_;
+    my ( $edges, $start_node, $end_node, $moves, $target, $min, $max ) = @_;
 
-    my $node = $head;
+    my $node = $start_node;
 
-    my @path = ( $head->{id} );
+    my @path = ($node);
 
     my %cache;
 
-    while ( $node->{id} ne $end_node->{id} ) {
-        my $next_nodes = get_next_nodes( $node, $moves );
+    while ( $node ne $end_node ) {
+        my $next_nodes = get_next_nodes( $edges, $node, $moves );
 
         my @counts;
 
         foreach my $next_node (@$next_nodes) {
-            my $paths = dfs( $next_node, $end_node, $moves, \%cache );
+            my $paths = dfs( $edges, $next_node, $end_node, $moves, \%cache );
             push @counts, { node => $next_node, paths => $paths };
         }
 
         @counts = sort {
-            $b->{node}->{id} =~ /^S(\d+)_(\d+)$/;
+            $b->{node} =~ /^S(\d+)_(\d+)$/;
             my ( $b_id, $b_step ) = ( $1, $2 );
 
-            $a->{node}->{id} =~ /^S(\d+)_(\d+)$/;
+            $a->{node} =~ /^S(\d+)_(\d+)$/;
             my ( $a_id, $a_step ) = ( $1, $2 );
 
             return $a_id <=> $b_id || $a_step <=> $b_step;
@@ -139,7 +141,7 @@ sub find_path_no {
 
         foreach my $c (@counts) {
             if ( $c->{min} <= $target && $target <= $c->{max} ) {
-                push @path, $c->{node}->{id};
+                push @path, $c->{node};
                 $node = $c->{node};
                 $min  = $c->{min};
                 $max  = $c->{max};
@@ -177,13 +179,13 @@ for ( my $i = 0 ; $i <= $end ; $i++ ) {
 printf $dp[$end] . "\n";
 
 # part 2
-my ( $head, $end_node ) = create_graph( \@lines );
-my $paths = dfs( $head, $end_node, \@moves, {} );
+my ( $edges, $start_node, $end_node ) = create_graph( \@lines );
+my $paths = dfs( $edges, $start_node, $end_node, \@moves, {} );
 print "$paths\n";
 
 # part 3
 my $target = Math::BigInt->new(100000000000000000000000000000);
 my $path =
-  find_path_no( $head, $end_node, \@moves, $target, Math::BigInt->new(1),
-    $paths );
+  find_path_no( $edges, $start_node, $end_node, \@moves, $target,
+    Math::BigInt->new(1), $paths );
 print "$path\n";
